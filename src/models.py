1c4691f0ea6c7df35815174dfec2fc270845deaf
LukasOy <lukas.oylu@gmail.com>
2022-12-22 22:47:30 +0000

    tareatermimnada

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,24 +8,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class UsuarioPrincipal(Base):
+    __tablename__ = 'Usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    NombreUsuario = Column(String(250), nullable=False)
+    SobreNombreUsuario = Column(String(10), nullable=False)
+    Mailusuario = Column(String(15), nullable=False)
+    FechaSuscripcion = Column(String(15), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class PersonajeFavorito(Base):
+    __tablename__ = 'PerFav'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Usuario_id = Column(Integer, ForeignKey('Usuario.id'))
+    NombrePersonaje = Column(String(20), nullable=False)
+    Personaje_id = Column(Integer, ForeignKey('Personaje.id'))
 
+class PlanetaFavorito(Base):
+    __tablename__ = 'PlaFav'
+    id = Column(Integer, primary_key=True)
+    Usuario_id = Column(Integer, ForeignKey('Usuario.id'))
+    NombrePlaneta = Column(String(15), nullable=False)
+    Planeta_id = Column(Integer, ForeignKey('Planeta.id'))
+
+class Personajes(Base):
+    __tablename__ = 'Personaje'
+    id = Column(Integer, primary_key=True)
+    NombrePersonaje = Column(String(20), nullable=False)
+    EdadPersonaje = Column(Integer, nullable=False)
+    FechaNacimiento = Column(DateTime(timezone=True), nullable=False)
+
+class Planeta(Base):
+    __tablename__='Planeta'
+    id = Column(Integer, primary_key=True)
+    Localizacion = Column(String(10), nullable=False)
+    
     def to_dict(self):
         return {}
 
